936c9dcc0453d86b7bd69c64574b046a2e1e2604
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-03-26 08:15:43 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId = 144567
+let accountEmail = " abc@gmail.com"
+var accountPassward = " 1234455"
+accountState = " Vadodara"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassward, accountState])
