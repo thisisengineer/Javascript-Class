18c13ace4880eb1e2a56d7c64439e464ffad689d
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-03-26 12:51:12 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,26 @@
+// Datatypes Def and Types
+"use strict" // treat all JS code as new version 
+//alert(3 + 3) we are using node js not browser
+console.log(3 + 3)
+console.log("Code Only")
+
+let name = 'Developer'  // string type
+let age = 21            // integer type
+let isLoggedIn = 'true'  // boolean type 
+let state ; // It returns undefined value
+// {Primitive Data-Types}
+
+//number       => range = 2 to the power 52
+//bigint       =>
+//string       =>  ""
+//boolean      => true/false
+//null         => standalone value
+//undefined    => means value is not assined now 
+//symbol       => to defined uniqueness 
+
+
+console.log(typeof "string")  // it return string
+console.log(typeof null)      // it return object
+console.log(typeof undefined) // it return  undefined 
+
+// Object
