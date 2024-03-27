6f8a46e4894c3fb9c6bdb807a01179ac28147f53
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-03-27 04:51:18 +0000

    Conversion

---
+++ 01_basics/conversionOperator.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+console.log(score)
+console.log(typeof score )
+console.log(typeof (score ))
+let valueInNumber = Number(score) // This will always return only integer type
+console.log(typeof valueInNumber) 
+console.log(valueInNumber)
+
+let isLoggedin = true;
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn)
+
+
+//Conversion One datatype to another datatype
+
+let number = 11
+let string = String(number);
+console.log(string)
+console.log(typeof string)
