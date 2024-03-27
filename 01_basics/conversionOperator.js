a94f5a28904879f327b09548e5e4909cf314beb6
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-03-27 05:30:59 +0000

    Conversion

--- 01_basics/conversionOperator.js
+++ 01_basics/conversionOperator.js
@@ -1,20 +1,63 @@
-let score = "33"
+// let score = "33"
 
-console.log(score)
-console.log(typeof score )
-console.log(typeof (score ))
-let valueInNumber = Number(score) // This will always return only integer type
-console.log(typeof valueInNumber) 
-console.log(valueInNumber)
+// console.log(score)
+// console.log(typeof score )
+// console.log(typeof (score ))
+// let valueInNumber = Number(score) // This will always return only integer type
+// console.log(typeof valueInNumber) 
+// console.log(valueInNumber)
 
-let isLoggedin = true;
-let booleanIsLoggedIn = Boolean(isLoggedin)
-console.log(booleanIsLoggedIn)
+// let isLoggedin = true;
+// let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn)
 
 
 //Conversion One datatype to another datatype
 
-let number = 11
-let string = String(number);
-console.log(string)
-console.log(typeof string)
+// let number = 11
+// let string = String(number);
+// console.log(string)
+// console.log(typeof string)
+
+//**************************************OPERATIONS*******************************************
+
+// Basics Operation 
+// console.log(4 + 4)
+// console.log(4 - 4)
+// console.log(4 * 4)
+// console.log(4 / 4)
+// console.log(4 ** 4)
+// console.log(4 % 4)
+
+// let str1 = "Abhi"
+// let str2 = "Pandit"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// Bad Practice never write this type of code
+
+// console.log( 1 + 2)
+// console.log( 1 + "2")
+// console.log( "2" + 2)
+// console.log( "1" +2 + 3)
+// console.log( 1 + 2 + "2")
+
+
+// Increment and decrement operator
+let gameCounter1 = 100
+gameCounter1++;
+console.log(gameCounter1)
+
+let gameCounter2 = 100
+gameCounter2--;
+console.log(gameCounter2)
+
+let gameCounter3 = 100
+++gameCounter3;
+console.log(gameCounter3)
+
+let gameCounter4 = 100
+--gameCounter4;
+console.log(gameCounter4)
+
+
