611f5965c068449d8f4581d876fe861202b267e1
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-03-30 04:35:41 +0000

    Math and Number

--- 01_basics/nums_and_maths.js
+++ 01_basics/nums_and_maths.js
@@ -1,37 +1,184 @@
 //***************All about numbers************************ */
 
-const score = 400; // this can return given datatypes[400]
-console.log(score)
+// const score = 400; // this can return given datatypes[400]
+// console.log(score)
 
-const num = new Number(100); // its return only specially number because here used number [Number: 100]
-console.log(num)
+// const num = new Number(100); // its return only specially number because here used number [Number: 100]
+// console.log(num)
 
-//some interesting things 
-console.log(num.toString()) // it will convert number to string :
-console.log(num.toString().length) // .lenth return the no. of character used:
-console.log(num.toFixed(4))//this return fixed value ex:- 100.0000
+// //some interesting things 
+// console.log(num.toString()) // it will convert number to string :
+// console.log(num.toString().length) // .lenth return the no. of character used:
+// console.log(num.toFixed(4))//this return fixed value ex:- 100.0000
 
-const newNum = 20.89
-console.log(newNum.toPrecision(2))
+// const newNum = 20.89
+// console.log(newNum.toPrecision(2))
 
-const myNum = 1000000000000000;
-console.log(myNum.toLocaleString('en-IN'))
+// const myNum = 1000000000000000;
+// console.log(myNum.toLocaleString('en-IN'))
 
 
 
-//****************************All about Maths****************************** */
+// //****************************All about Maths****************************** */
 
-console.log(Math) // this console return like Object[Math] {}
-console.log(Math.abs(-4))
-console.log(Math.round(4.9)) // ceil and floor also becomes use in math console to round off the number 
-console.log(Math.sqrt(100))
+// console.log(Math) // this console return like Object[Math] {}
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.9)) // ceil and floor also becomes use in math console to round off the number 
+// console.log(Math.sqrt(100))
 
-console.log(Math.random())
-console.log((Math.random() * 10) + 1)
-console.log(Math.floor(Math.random() * 10) + 1)
+// console.log(Math.random())
+// console.log((Math.random() * 10) + 1)
+// console.log(Math.floor(Math.random() * 10) + 1)
 
-const min = 10;
-const max = 20;
+// const min = 10;
+// const max = 20;
 
-console.log(Math.floor(Math.random) * (max - min + 1) + min)
+// console.log(Math.floor(Math.random) * (max - min + 1) + min)
+// below all are math functions 
+   
+// E
+// : 
+// 2.718281828459045
+// LN2
+// : 
+// 0.6931471805599453
+// LN10
+// : 
+// 2.302585092994046
+// LOG2E
+// : 
+// 1.4426950408889634
+// LOG10E
+// : 
+// 0.4342944819032518
+// PI
+// : 
+// 3.141592653589793
+// SQRT1_2
+// : 
+// 0.7071067811865476
+// SQRT2
+// : 
+// 1.4142135623730951
+// abs
+// : 
+// ƒ abs()
+console.log(Math.abs(-8))
+// acos
+
+// : 
+// ƒ acos()
+console.log(Math.acos(1))
+console.log(Math.acos(0))
+console.log(Math.acos(-1))
+// acosh
+// : 
+// ƒ acosh() 
+console.log(Math.acosh(30))
+// asin
+// : 
+// ƒ asin()
+console.log(Math.asin(3))
+// asinh
+// : 
+// ƒ asinh()
+// atan
+// : 
+// ƒ atan()
+// atan2
+// : 
+// ƒ atan2()
+// atanh
+// : 
+// ƒ atanh()
+// cbrt
+// : 
+// ƒ cbrt()
+// ceil
+// : 
+// ƒ ceil()
+// clz32
+// : 
+// ƒ clz32()
+// cos
+// : 
+// ƒ cos()
+// cosh
+// : 
+// ƒ cosh()
+// exp
+// : 
+// ƒ exp()
+// expm1
+// : 
+// ƒ expm1()
+// floor
+// : 
+// ƒ floor()
+// fround
+// : 
+// ƒ fround()
+// hypot
+// : 
+// ƒ hypot()
+// imul
+// : 
+// ƒ imul()
+// log
+// : 
+// ƒ log()
+// log1p
+// : 
+// ƒ log1p()
+// log2
+// : 
+// ƒ log2()
+// log10
+// : 
+// ƒ log10()
+// max
+// : 
+// ƒ max()
+// min
+// : 
+// ƒ min()
+// pow
+// : 
+// ƒ pow()
+// random
+// : 
+// ƒ random()
+// round
+// : 
+// ƒ round()
+// sign
+// : 
+// ƒ sign()
+// sin
+// : 
+// ƒ sin()
+// sinh
+// : 
+// ƒ sinh()
+// sqrt
+// : 
+// ƒ sqrt()
+// tan
+// : 
+// ƒ tan()
+// tanh
+// : 
+// ƒ tanh()
+// trunc
+// : 
+// ƒ trunc()
+// Symbol(Symbol.toStringTag)
+// : 
+// "Math"
+// [[Prototype]]
+// : 
+// Object
+// ﻿
+
+
 
