403392481ffa2fbb34f610deecb1c5bf12c6d105
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-03-30 04:36:32 +0000

    Math

--- 01_basics/nums_and_maths.js
+++ 01_basics/nums_and_maths.js
@@ -1,39 +1,39 @@
 //***************All about numbers************************ */
 
-// const score = 400; // this can return given datatypes[400]
-// console.log(score)
+const score = 400; // this can return given datatypes[400]
+console.log(score)
 
-// const num = new Number(100); // its return only specially number because here used number [Number: 100]
-// console.log(num)
+const num = new Number(100); // its return only specially number because here used number [Number: 100]
+console.log(num)
 
-// //some interesting things 
-// console.log(num.toString()) // it will convert number to string :
-// console.log(num.toString().length) // .lenth return the no. of character used:
-// console.log(num.toFixed(4))//this return fixed value ex:- 100.0000
+//some interesting things 
+console.log(num.toString()) // it will convert number to string :
+console.log(num.toString().length) // .lenth return the no. of character used:
+console.log(num.toFixed(4))//this return fixed value ex:- 100.0000
 
-// const newNum = 20.89
-// console.log(newNum.toPrecision(2))
+const newNum = 20.89
+console.log(newNum.toPrecision(2))
 
-// const myNum = 1000000000000000;
-// console.log(myNum.toLocaleString('en-IN'))
+const myNum = 1000000000000000;
+console.log(myNum.toLocaleString('en-IN'))
 
 
 
-// //****************************All about Maths****************************** */
+//****************************All about Maths****************************** */
 
-// console.log(Math) // this console return like Object[Math] {}
-// console.log(Math.abs(-4))
-// console.log(Math.round(4.9)) // ceil and floor also becomes use in math console to round off the number 
-// console.log(Math.sqrt(100))
+console.log(Math) // this console return like Object[Math] {}
+console.log(Math.abs(-4))
+console.log(Math.round(4.9)) // ceil and floor also becomes use in math console to round off the number 
+console.log(Math.sqrt(100))
 
-// console.log(Math.random())
-// console.log((Math.random() * 10) + 1)
-// console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
 
-// const min = 10;
-// const max = 20;
+const min = 10;
+const max = 20;
 
-// console.log(Math.floor(Math.random) * (max - min + 1) + min)
+console.log(Math.floor(Math.random) * (max - min + 1) + min)
 // below all are math functions 
    
 // E
