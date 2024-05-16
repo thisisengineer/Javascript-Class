917e6b1d4b169b97187aef1031d20bc3823ff45a
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-05-16 06:17:43 +0000

    loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,13 @@
+// while loop 
+let i = 0;
+while(i <= 10){
+    console.log(` count is ${i}`)
+    i = i + 2;
+}
+
+// do while loop
+let score = 1;
+do {
+    console.log(`value is ${score}`)
+    score++;
+} while(score <= 10)
